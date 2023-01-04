e6b7792f597d0cf59e28cfd6e6aefcc7bc5ceec5
UnTiTled <104919147+NikhilBero@users.noreply.github.com>
2023-01-04 15:20:05 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let playerTurn = "X"
+let gameOver = false
+// function to change turn
+
+const changeTurn = () =>{
+
+return  playerTurn === "X"?"0" : "X" ;
+
+}
+
+// Function to check win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            gameOver = true
+           playerTurn = ""
+        }
+    })
+}
+
+// Game Logic 
+
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = playerTurn;
+            playerTurn = changeTurn();
+           
+            checkWin();
+            if (!gameOver){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " +playerTurn;
+               
+            } 
+        }
+    })
+})
+
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    gameOver = false
+   
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+   
+})
