59335720b8b79dacbac87d70c2532654426aca39
UnTiTled <104919147+NikhilBero@users.noreply.github.com>
2023-01-05 13:36:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ return  playerTurn === "X"?"0" : "X" ;
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ]
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
@@ -55,7 +55,7 @@ reset.addEventListener('click', ()=>{
     Array.from(boxtexts).forEach(element => {
         element.innerText = ""
     });
-    turn = "X"; 
+    playerTurn = "X"; 
     gameOver = false
    
     document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
